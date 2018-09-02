d9f9e36038dfc6e95d156c0ce5748d74c5bc41ab
Antoine Albertelli <antoine@antoinealb.net>
2018-09-02 11:17:10 +0200

    Finish data logging

--- battery_logger/battery_logger/battery_logger.ino
+++ battery_logger/battery_logger/battery_logger.ino
@@ -1,33 +1,48 @@
 #include "drotek_sensor.h"
 #include "LowPassFilter.h"
 
+#define LED_PIN 13
+#define PERIOD_TRIM 9
+
 const float sample_freq = 1000;
+
 
 // We are recording at 1 Hz, so we need to cutoff below 1/2 Hz to
 // respect Shannon limit
-const float cutoff_freq = 0.25;
+const float cutoff_freq = 0.1;
 
 LowPassFilter current_lpf(sample_freq, cutoff_freq);
 LowPassFilter voltage_lpf(cutoff_freq, cutoff_freq);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("time_s,current_a,voltage_v");
+  Serial.println("time_ms,current_ma,voltage_mv");
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   static int sample_index = 0;
+  static bool led_status = false;
+
+  int start_us = micros();
+
   int v_adc = analogRead(0);
   int i_adc = analogRead(1);
 
+  // Filter the samples
   float i = current_lpf(i_adc);
   float u = voltage_lpf(v_adc);
 
+  // Write down result every second
   if (sample_index ++ == 1000) {
     char line[128];
-    sprintf(line, "%d,%.2f,%.2f", millis(), i, u);
+    sprintf(line, "%lu,%ld,%ld", millis(), static_cast<long>(1000 * i), static_cast<long>(1000 * u));
     Serial.println(line);
     sample_index = 0;
+    led_status = !led_status;
+    digitalWrite(LED_PIN, led_status);
   }
-  delayMicroseconds(1000);
+
+  int stop_us = micros();
+  delayMicroseconds(1000 - PERIOD_TRIM - (stop_us - start_us));
 }
